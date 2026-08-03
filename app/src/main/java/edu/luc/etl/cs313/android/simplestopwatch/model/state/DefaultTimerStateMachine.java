03bcca75982973a80fc4a8c01781030a2a76a425
sandrasoza <ssozazambrano@luc.edu>
2026-08-03 14:38:14 -0500

    Updated from DefaultStopwatchtateMachine to DefaultTimerStateMachine

---
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/DefaultTimerStateMachine.java
@@ -0,0 +1,89 @@
+package edu.luc.etl.cs313.android.simplestopwatch.model.state;
+
+import edu.luc.etl.cs313.android.simplestopwatch.common.StopwatchModelListener;
+import edu.luc.etl.cs313.android.simplestopwatch.model.clock.ClockModel;
+import edu.luc.etl.cs313.android.simplestopwatch.model.time.TimeModel;
+import edu.luc.etl.cs313.android.simplestopwatch.model.time.TimeoutModel;
+
+/**
+ * An implementation of the state machine for the timer.
+ * Extended for Project 4 to also depend on a TimeoutModel, used by
+ * SettingState to detect 3 seconds of inactivity.
+ * @author laufer
+ */
+public class DefaultTimerStateMachine implements StopwatchStateMachine {
+
+    public DefaultTimerStateMachine(final TimeModel timeModel, final ClockModel clockModel,
+                                    final TimeoutModel timeoutModel) {
+        this.timeModel = timeModel;
+        this.clockModel = clockModel;
+        this.timeoutModel = timeoutModel;
+        this.timeoutModel.setOnTimeout(this::onTimeout);
+    }
+
+    private final TimeModel timeModel;
+
+    private final ClockModel clockModel;
+
+    private final TimeoutModel timeoutModel;
+
+    /**
+     * The internal state of this adapter component. Required for the State pattern.
+     */
+    private StopwatchState state;
+
+    protected void setState(final StopwatchState state) {
+        this.state = state;
+        listener.onStateUpdate(state.getId());
+    }
+
+    private StopwatchModelListener listener;
+
+    @Override
+    public void setModelListener(final StopwatchModelListener listener) {
+        this.listener = listener;
+    }
+
+    // forward event uiUpdateListener methods to the current state
+    // these must be synchronized because events can come from the
+    // UI thread or a timer thread (clock tick or inactivity timeout)
+    @Override public synchronized void onStartStop() { state.onStartStop(); }
+    @Override public synchronized void onLapReset()  { state.onLapReset(); }
+    @Override public synchronized void onTick()      { state.onTick(); }
+    @Override public synchronized void onTimeout()   { state.onTimeout(); }
+
+    @Override public void updateUIRuntime() { listener.onTimeUpdate(timeModel.getTime()); }
+
+    // known states
+    private final StopwatchState STOPPED  = new StoppedState(this);
+    private final StopwatchState SETTING  = new SettingState(this);
+    private final StopwatchState RUNNING  = new RunningState(this);
+    private final StopwatchState ALARMING = new AlarmingState(this);
+
+    // transitions
+    @Override public void toStoppedState()  { setState(STOPPED); }
+    @Override public void toSettingState()  { setState(SETTING); }
+    @Override public void toRunningState()  { setState(RUNNING); }
+    @Override public void toAlarmingState() { setState(ALARMING); }
+
+    // actions
+    @Override public void actionInit()  { toStoppedState(); actionReset(); }
+    @Override public void actionReset() { timeModel.resetTime(); actionUpdateView(); }
+    @Override public void actionInc()   { timeModel.incTime(); actionUpdateView(); }
+    @Override public void actionDec()   { timeModel.decTime(); actionUpdateView(); }
+
+    @Override public void actionStart() { clockModel.start(); }
+    @Override public void actionStop()  { clockModel.stop(); }
+
+    @Override public void actionStartTimeout() { timeoutModel.start(); }
+    @Override public void actionStopTimeout()  { timeoutModel.stop(); }
+
+    //wire to a real alarm/sound mechanism
+    @Override public void actionAlarmOn()  { }
+    @Override public void actionAlarmOff() { }
+
+    @Override public void actionUpdateView() { state.updateView(); }
+
+    // guards
+    @Override public boolean isTimeZero() { return timeModel.isZero(); }
+}
--- app/src/test/java/edu/luc/etl/cs313/android/simplestopwatch/test/model/DefaultTimerStateMachineTest.java
+++ app/src/test/java/edu/luc/etl/cs313/android/simplestopwatch/test/model/DefaultTimerStateMachineTest.java
@@ -3,7 +3,7 @@ package edu.luc.etl.cs313.android.simplestopwatch.test.model;
 import org.junit.After;
 import org.junit.Before;
 
-import edu.luc.etl.cs313.android.simplestopwatch.model.state.DefaultStopwatchStateMachine;
+import edu.luc.etl.cs313.android.simplestopwatch.model.state.DefaultTimerStateMachine;
 
 /**
  * Concrete testcase subclass for the default stopwatch state machine
@@ -12,12 +12,12 @@ import edu.luc.etl.cs313.android.simplestopwatch.model.state.DefaultStopwatchSta
  * @author laufer
  * @see http://xunitpatterns.com/Testcase%20Superclass.html
  */
-public class DefaultStopwatchStateMachineTest extends AbstractStopwatchStateMachineTest {
+public class DefaultTimerStateMachineTest extends AbstractStopwatchStateMachineTest {
 
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        setModel(new DefaultStopwatchStateMachine(getDependency(), getDependency()));
+        setModel(new DefaultTimerStateMachine(getDependency(), getDependency()));
     }
 
     @After
